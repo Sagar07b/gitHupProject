61b9327cbeca741c1fefe69f75d11868abcf12f2
Sagae Bhor <sagarbhor011@gmail.com>
2026-01-17 21:17:18 +0530

    Home Page Script

--- src/test/java/Testing/TestingClass1.java
+++ src/test/java/Testing/TestingClass1.java
@@ -5,9 +5,9 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import setup.Base;
+import setup.DriverManager;
 
-public class TestingClass1 extends Base {
+public class TestingClass1 extends DriverManager {
 
 	@BeforeMethod
 	public void launchingBrowser() throws InterruptedException {
